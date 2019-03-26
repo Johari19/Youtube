38952c5f52256812c2b3cb6cdf84c06942481fcf
Joe <46333948+Johari19@users.noreply.github.com>
2019-03-25 20:35:44 -0400

    Add files via upload

---
+++ Modules/calc.py
@@ -0,0 +1,10 @@
+from main import *
+
+x = 4
+y = 9
+text = "This is a basic calculator"
+
+printStuff(text)
+add(x,y)
+subtract(x,y)
+multiply(x,y)
---
+++ Modules/main.py
@@ -0,0 +1,11 @@
+def add(x,y):
+    print(x + y)
+
+def subtract(x,y):
+    print(x - y)
+
+def multiply(x,y):
+    print(x * y)
+
+def printStuff(text):
+    print(text)
