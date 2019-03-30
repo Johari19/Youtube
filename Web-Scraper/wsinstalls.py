e6080dd75408c7d17c6d16b1c2b880bbb9d793e4
Joe <46333948+Johari19@users.noreply.github.com>
2019-03-30 19:20:20 -0400

    Update wsinstalls.py

--- Web-Scraper/wsinstalls.py
+++ Web-Scraper/wsinstalls.py
@@ -2,11 +2,6 @@ from subprocess import call, DEVNULL
 import subprocess
 import time
 import os
-from requests import get
-from requests.exceptions import RequestException
-from contextlib import closing
-from bs4 import BeautifulSoup
-
 if __name__ == "__main__":
     print("DO NOT RUN THIS SCRIPT! IT DOES NOTHING")
     time.sleep(10)
@@ -19,18 +14,20 @@ else:
         if __name__ == "wsinstalls":
             print("Installing requests modules...")
             install('requests')
-            time.sleep(2)
+            time.sleep(1)
             print("Installing beautifulsoup4 modules...")
             install("beautifulsoup4")
-            time.sleep(2)
+            time.sleep(1)
             print("Installed all modules!")
             print("Importing modules...")
+
+
             from requests import get
             from requests.exceptions import RequestException
             from contextlib import closing
             from bs4 import BeautifulSoup
-            import os
-            time.sleep(2)
+
+            time.sleep(1)
             print("Imported all modules!")
         else:
             print("Installation process failed!")
